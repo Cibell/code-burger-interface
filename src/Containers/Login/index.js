98b4bc3f0bf455b7fe44e02779b650d3e11579de
Cibele <cibelecordeirolopes@gmail.com>
2022-11-11 20:26:44 -0300

    styling the login page

--- src/Containers/Login/index.js
+++ src/Containers/Login/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+import LoginImg from '../../assets/login-imag.svg'
+import Logo from '../../assets/logo.svg'
+import {
+  Container,
+  LoginImage,
+  ContainerItem,
+  Label,
+  Input,
+  Button,
+  SignInLink
+} from './styles'
+
 function Login () {
   return (
-        <div>
+        <Container>
+          <LoginImage src={LoginImg} alt="login-image"/>
+          <ContainerItem>
+            <img src={Logo} alt="logo"/>
             <h1>Login</h1>
-        </div>
+
+            <Label>Email</Label>
+            <Input/>
+
+            <Label>Password</Label>
+            <Input/>
+
+            <Button>Sign In</Button>
+            <SignInLink>Não possui conta? <a>Sign up</a></SignInLink>
+
+          </ContainerItem>
+
+        </Container>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Login from './Containers/Login'
+import StyledGlobal from './styled/styledGlobal'
 
 const root =
 ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <StyledGlobal/>
     <Login />
   </React.StrictMode>
 )
